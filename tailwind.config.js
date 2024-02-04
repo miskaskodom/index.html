cbb7f988538100053f2ded0e4836f574543619c2
Dmitri <dima.okey@gmail.com>
2024-02-04 20:07:33 +0300

    Project1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+    current: 'currentColor',
+      'Black' : '#27272A',
+      'Greydark' : '#696969',
+      'Greylight' : '#E5E7EB',
+      'White' : '#FCFBFA',
+      'Lavender' : '#7E3AF2',
+      ALERTRED : '#E02424',
+    },
+    fontFamily: {
+      mont :[ 'Montserrat'],
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+}
